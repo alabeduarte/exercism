e991325857ebb2ec0172fbf788a8a3e96a879116
Alabê Duarte <alabeduarte@gmail.com>
2017-05-29 15:50:44 +1000

    Replace math function calls by a simple '%'

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,16 +1,10 @@
 package leap
 
-import "math"
-
 const testVersion = 3
 
-func IsDivisible(number int, divisor int) bool {
-  return math.Mod(float64(number), float64(divisor)) == 0
-}
-
 func IsLeapYear(year int) bool {
-  if IsDivisible(year, 4) && !IsDivisible(year, 100) ||
-    IsDivisible(year, 100) && IsDivisible(year, 400) {
+  if year % 4 == 0 && year % 100 != 0 ||
+    year % 100 == 0 && year % 400 == 0 {
     return true
   }
 

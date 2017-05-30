28f89d93f344832699a91192f37d844e45472c38
Alabê Duarte <alabeduarte@gmail.com>
2017-05-30 13:51:33 +1000

    Refactoring if statement to be single line

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -3,10 +3,6 @@ package leap
 const testVersion = 3
 
 func IsLeapYear(year int) bool {
-  if year % 4 == 0 && year % 100 != 0 ||
-    year % 100 == 0 && year % 400 == 0 {
-    return true
-  }
-
-  return false
+  return (year % 4 == 0 && year % 100 != 0) ||
+    (year % 100 == 0 && year % 400 == 0)
 }

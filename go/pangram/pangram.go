d8cd334f4d6ad5f7d2ccfc8558e5139b02e17280
Alabê Duarte <alabeduarte@gmail.com>
2017-07-26 16:02:48 +1000

    Using a simpler solution for pangram problem

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -4,35 +4,15 @@ import "strings"
 
 const testVersion = 1
 
-func ToMap(array []string) map[string]string {
-	mapOfElements := make(map[string]string)
-
-	for _, elem := range array {
-		mapOfElements[elem] = elem
-	}
-
-	return mapOfElements
-}
-
 func IsPangram(phrase string) bool {
-	alphabet := ToMap(strings.Split("ABCDEFGHIJKLMNOPQRSTUVWXYZ", ""))
+	alphabet := "abcdefghijklmnopqrstuvwxyz"
+	phrase = strings.ToLower(phrase)
 
-	phrase = strings.ToUpper(strings.Replace(phrase, " ", "", -1))
-	phraseArray := strings.Split(phrase, "")
-
-	charactersFound := make(map[string]string)
-
-	for _, elem := range phraseArray {
-		_, exist := alphabet[elem]
-
-		if exist {
-			charactersFound[elem] = elem
+	for _, character := range alphabet {
+		if !strings.Contains(phrase, string(character)) {
+			return false
 		}
 	}
 
-	if len(charactersFound) == len(alphabet) {
-		return true
-	}
-
-	return false
+	return true
 }
